7f16b1ce48c6b618341776eee282c07b177ed853
rvicens <rvicens@example.com>
2015-08-22 23:28:12 +0200

    Created TelegramMessageProcessor class

--- BolsaTrackerBot/bolsa_tracker.py
+++ BolsaTrackerBot/bolsa_tracker.py
@@ -3,44 +3,29 @@
 import time
 import logging
 import telegram
+from lib.TelegramMessageProcessor.TelegramMessageProcessor import *
 
-
-class MessageProcessor():
-    def __init__(self):
-        self.msg = None
-
-    def extractMessage(self, tlg_msg):
-        self.msg = tlg_msg.message.text.encode('utf-8')
-
-    def validateMessage(self):
-        return True
-
-    def process(self, tlg_msg):
-        self.extractMessage(tlg_msg)
-        if self.validateMessage():
-            return self.msg
-        return None
-
-
-def processMessage(bot, last_update_id):
+def processMessage(bot, last_update_id,logger):
     for update in bot.getUpdates(offset=last_update_id):
         if last_update_id < update.update_id:
-
             # chat_id is required to reply any message
             chat_id = update.message.chat_id
-            mp = MessageProcessor()
+            logger.debug("Found new message with ID:{0} and Chat ID:{1}".format(update.update_id,chat_id))
+            mp = TelegramMessageProcessor()
             message = mp.process(update)
 
             if (message):
                 # Reply the message
                 bot.sendMessage(chat_id=chat_id, text=message)
-
                 # Returns global offset to get the new updates
+                logger.debug("Finished processing message with ID:{0} and Chat ID:{1}".format(update.update_id,chat_id))
                 return update.update_id
+            logger.debug("Message Seems to be None")
+
     return None
 
 
-def main():
+def main(logger):
     LAST_UPDATE_ID = None
 
     # Telegram Bot Authorization Token
@@ -48,17 +33,27 @@ def main():
 
     # for updates. It starts with the latest update_id if available.
     try:
+        logger.debug("Getting Telegram updates")
         LAST_UPDATE_ID = bot.getUpdates()[-1].update_id
+        logger.debug("Last Update ID:{0}".format(str(LAST_UPDATE_ID)))
     except IndexError:
         LAST_UPDATE_ID = None
+        logger.exeption("ERROR retrieving update id")
 
     while True:
-        messageResult = processMessage(bot, LAST_UPDATE_ID)
+        messageResult = processMessage(bot, LAST_UPDATE_ID,logger)
         if messageResult:
             LAST_UPDATE_ID = messageResult
         time.sleep(3)
 
 
 if __name__ == '__main__':
-    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    main()
+
+    logger = logging.getLogger('Main')
+    hdlr = logging.FileHandler('logs/bolsatracker.log')
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    hdlr.setFormatter(formatter)
+    logger.addHandler(hdlr)
+    logger.setLevel(logging.DEBUG)
+
+    main(logger)
