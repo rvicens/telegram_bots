65e2b8bb7c9cfd3a3eb212f1ee7f4263d305be24
rvicens <rvicens@github.lan>
2015-09-27 00:30:06 +0200

    Finished HR plugin

--- BolsaTrackerBot/plugins/hr.py
+++ BolsaTrackerBot/plugins/hr.py
@@ -1,7 +1,7 @@
 import logging
 import telegram
 from yapsy.IPlugin import IPlugin
-from lib.stockQuotesAPI.pcbolsaSearch import *
+from lib.cnmvAPI import *
 
 class Check(IPlugin):
 
@@ -9,16 +9,35 @@ class Check(IPlugin):
     command = "/hr"
     description = "Plugin to check 'Hechos Relevantes'. e.g. /hr"
 
+
+    def getCompanies(self,msg):
+        out = []
+        msgs = msg.split(",")
+        for item in msgs:
+            if item:
+                out.append(item.replace(" ","").upper())
+        return out
+
     def run(self,msg):
 
         results = {"text":"","replay_markup":""}
 
         logger = logging.getLogger("Main.HR")
         logger.debug("Running HR plugin")
+        companies = self.getCompanies(msg)
+        cnmv = cnmvAPI()
 
+        if len(companies) > 0:
+            results["text"] = cnmv.getComapanyHR(companies)
+            results["replay_markup"] = None
+        else:
+            results["text"] = "Try the following companies typing:\n\n"
+            custom_keyboard = []
+            for key in sorted(ecob.companies.keys()):
+                custom_keyboard.append(["/hr {0}".format(key)])
 
-        results["text"] = ""
-        results["replay_markup"] = None
+            results["replay_markup"] = telegram.ReplyKeyboardMarkup(custom_keyboard)
 
         logger.debug("Finished HR plugin")
+
         return results
